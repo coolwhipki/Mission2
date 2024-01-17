93854b97b7d536f5297c2447bf815742eb4845f3
coolwhipki <kirajanegirl@gmail.com>
2024-01-17 16:43:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Creating second class
+        Slay s = new Slay();
+
+        System.Console.WriteLine("\nWelcome to the dice throwing simulator!\n");
+
+        //input from user
+        System.Console.WriteLine("How many dice rolls would you like to stimulate");
+        int numRolls = int.Parse(System.Console.ReadLine());
+
+        //Pass NumRolls to second class
+        int[] resultArray = s.RollDie(numRolls);
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULT\nEach '*' represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + numRolls);
+
+        //Print the histogram and the results
+        
+        for (int eachCount = 0; eachCount < resultArray.Length; eachCount++)
+        {
+            // calculates the number of stars from rolls
+            int numStars = (int)Math.Round(((double)resultArray[eachCount] / numRolls) * 100);
+
+            //create a string of stars based on the number of stars
+            string starString = new string('*', numStars);
+
+            // list the numbers and the stars
+            System.Console.WriteLine((eachCount + 2) + ": " + starString);
+
+        }
+
+        System.Console.WriteLine("Thank you for playing the dice throwing simulator. Goodbye!");
+    }
+
+
+}
---
+++ Slay.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class Slay
+    {
+        public Slay()
+        {
+
+        }
+        public int[] RollDie(int rollz)
+        {
+            // import the random package so we can roll die randomly
+            Random random = new Random();
+
+            // Array that holds the count of the comination of the two dices 
+            int[] rollNumCounts = new int[11];
+
+
+            // Dice roll for specified times and count number of times 
+            for (int count = 0; count < rollz; count++)
+            {
+
+                int die1 = random.Next(1, 7);
+                int die2 = random.Next(1, 7);
+
+                int sumMaddness = die1 + die2;
+
+                rollNumCounts[sumMaddness - 2]++;
+            }
+
+            return rollNumCounts;
+        }
+    }
+}
